9d2ca1e1fcbcad4e7b91e0d0c2906f6c6ec0195a
Technote <technote.space@gmail.com>
2020-03-08 22:02:33 +0900

    feat: add sample code (#167)

--- src/process.ts
+++ src/process.ts
@@ -4,10 +4,21 @@ import { Logger, Command } from '@technote-space/github-action-helper';
 import { getPayload } from './utils/misc';
 
 export const execute = async(logger: Logger, octokit: Octokit, context: Context): Promise<void> => {
+	logger.startProcess('Dump context');
 	console.log(getPayload(context));
 
+	logger.startProcess('Command test');
 	const command = new Command(logger);
 	await command.execAsync({
 		command: 'ls -lat',
 	});
+
+	logger.startProcess('Color text');
+	logger.info('green text: %s', logger.c('green', {color: 'red', attribute: 'bold'}));
+	logger.warn('warning!');
+	logger.error('error!!!');
+	logger.debug('debug...');
+	logger.log('log log log');
+
+	logger.endProcess();
 };
