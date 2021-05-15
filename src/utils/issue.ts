6f030d9b4fe5a0aeadf7d4453247257d49f28ffa
Technote <technote.space@gmail.com>
2021-05-15 16:19:37 +0900

    fix: octokit types

--- src/utils/issue.ts
+++ src/utils/issue.ts
@@ -1,12 +1,11 @@
 import {Context} from '@actions/github/lib/context';
 import {Octokit} from '@technote-space/github-action-helper/dist/types';
-import {PaginateInterface} from '@octokit/plugin-paginate-rest';
 import {components} from '@octokit/openapi-types';
 
 type IssuesListForRepoResponseData = components['schemas']['issue-simple'];
 
-export const getIssues = async(octokit: Octokit, context: Context): Promise<Array<IssuesListForRepoResponseData>> => (await (octokit.paginate as PaginateInterface)(
-  octokit.issues.listForRepo, {
+export const getIssues = async(octokit: Octokit, context: Context): Promise<Array<IssuesListForRepoResponseData>> => (await octokit.paginate(
+  octokit.rest.issues.listForRepo, {
     ...context.repo,
-  }
+  },
 )).map(item => item as IssuesListForRepoResponseData).filter(item => !('pull_request' in item));
