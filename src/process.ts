f6537de799d7b1d2aaa9a0b43afdabbc7ce036d4
Technote <technote.space@gmail.com>
2021-11-13 19:43:30 +0900

    chore: use type import

--- src/process.ts
+++ src/process.ts
@@ -1,7 +1,7 @@
-import {Context} from '@actions/github/lib/context';
-import {Octokit} from '@technote-space/github-action-helper/dist/types';
+import type {Context} from '@actions/github/lib/context';
+import type {Octokit} from '@technote-space/github-action-helper/dist/types';
+import type {Logger} from '@technote-space/github-action-log-helper';
 import {Command} from '@technote-space/github-action-helper';
-import {Logger} from '@technote-space/github-action-log-helper';
 import {getIssues} from './utils/issue';
 import {getPayload} from './utils/misc';
 
